824228065470e87ff0c1a4b486f440412b721b06
Erwin <erwinb1996@gmail.com>
2021-07-02 10:20:41 -0500

    CHANGELOG: 1. State management created with vuex 2. Refactored state management of the store with vuex

--- src/pages/ProductsList.vue
+++ src/pages/ProductsList.vue
@@ -22,14 +22,19 @@ export default {
   components: {
     ProductItem,
   },
+  computed: {
+    products() {
+      return this.$store.getters['prods/products'];
+    },
+  },
 };
 </script>
 
 <style scoped>
-  ul {
-    list-style: none;
-    margin: 2rem auto;
-    padding: 0;
-    max-width: 40rem;
-  }
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 40rem;
+}
 </style>
